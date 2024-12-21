6c74c8e652f415a7cb847b8aa9091258afa3518a
Jaxongir <jorajaxongir229@gmail.com>
2024-12-21 16:34:11 +0500

    soz oyin botiga ketik

--- filters/__init__.py
+++ filters/__init__.py
@@ -1,9 +1,14 @@
+#telegram/filters/__init__.py
 from aiogram import Dispatcher
 
 from loader import dp
-# from .is_admin import AdminFilter
-
-
+from .output import GetData,GetTXT
+#from .In_List import ListFilter,NotListFilter
+#from .IS_ADMIN import AdminFilter
 if __name__ == "filters":
-    #dp.filters_factory.bind(is_admin)
+    dp.filters_factory.bind(GetData)
+    dp.filters_factory.bind(GetTXT)
+    #dp.filters_factory.bind(ListFilter)
+    #dp.filters_factory.bind(AdminFilter)
+    #dp.filters_factory.bind(NotListFilter)
     pass
--- handlers/users/help.py
+++ handlers/users/help.py
@@ -3,8 +3,8 @@ from aiogram.dispatcher.filters.builtin import CommandHelp
 
 from loader import dp
 
-
-@dp.message_handler(CommandHelp())
+@dp.callback_query_handler(get_Data=False)
+@dp.message_handler(CommandHelp(),get_txt=True)
 async def bot_help(message: types.Message):
     text = ("Buyruqlar: ",
             "/start - Botni ishga tushirish",
